1d0dec463b36b8c4818a0b639ec74f07759a691d
JZorvan <jzorvan@gmail.com>
2016-10-15 14:04:15 -0700

    Never gonna give you up.

--- RepoQuiz/Migrations/Configuration.cs
+++ RepoQuiz/Migrations/Configuration.cs
@@ -13,21 +13,21 @@ namespace RepoQuiz.Migrations
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(RepoQuiz.DAL.StudentContext context)
-        {
-            context.Students.AddOrUpdate(
-                s = s.StudentID,
-                    new Student { FirstName = "Agnes", LastName = "Butts", Major = "Underwater Basket Weaving" },
-                    new Student { FirstName = "Ethel", LastName = "Hitler", Major = "Cannibis Agriculture" },
-                    new Student { FirstName = "Mildred", LastName = "Hooker", Major = "Fermentation Sciences" },
-                    new Student { FirstName = "Bertha", LastName = "Boner", Major = "Auctioneering" },
-                    new Student { FirstName = "Gertrude", LastName = "Wang", Major = "Canadian Studies" },
-                    new Student { FirstName = "Dick", LastName = "Head", Major = "Jazz Hands" },
-                    new Student { FirstName = "Angus", LastName = "Trumpizievil", Major = "Pupperty" },
-                    new Student { FirstName = "Herman", LastName = "McUgly", Major = "Kitten Management" },
-                    new Student { FirstName = "Mortimer", LastName = "Fuchs", Major = "Confection Distribution by Van" },
-                    new Student { FirstName = "Rupert", LastName = "Dickfloss", Major = "Petting Doggies" }
-                );
-        }
+       // protected override void Seed(RepoQuiz.DAL.StudentContext context)
+       // {
+           // context.Students.AddOrUpdate(
+             //   s = s.StudentID,
+               //     new Student { FirstName = "Agnes", LastName = "Butts", Major = "Underwater Basket Weaving" },
+                 //   new Student { FirstName = "Ethel", LastName = "Hitler", Major = "Cannibis Agriculture" },
+                   // new Student { FirstName = "Mildred", LastName = "Hooker", Major = "Fermentation Sciences" },
+                    //new Student { FirstName = "Bertha", LastName = "Boner", Major = "Auctioneering" },
+                    //new Student { FirstName = "Gertrude", LastName = "Wang", Major = "Canadian Studies" },
+                    //new Student { FirstName = "Dick", LastName = "Head", Major = "Jazz Hands" },
+                   // new Student { FirstName = "Angus", LastName = "Trumpizievil", Major = "Pupperty" },
+                   // new Student { FirstName = "Herman", LastName = "McUgly", Major = "Kitten Management" },
+                   // new Student { FirstName = "Mortimer", LastName = "Fuchs", Major = "Confection Distribution by Van" },
+                    //new Student { FirstName = "Rupert", LastName = "Dickfloss", Major = "Petting Doggies" }
+               /// );
+       // }
     }
 }
